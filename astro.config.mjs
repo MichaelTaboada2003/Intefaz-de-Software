eabcf8612fb0190fb2cbbd6ea2fc284229d60dfe
MichaelTaboada2003 <mtaboada@utb.edu.co>
2024-05-12 01:09:29 -0500

    Feat: preparation for inclusion of react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-// astro.config.mjs
+
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), react()],
 });
+
