27f9878664311dd9e6c6d97f7810f7960869615b
MichaelTaboada2003 <mtaboada@utb.edu.co>
2024-05-12 02:47:08 -0500

    Feat: Clickable images!

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,10 @@
-import React, { useRef } from 'react';
-import './Slider.css';  
+import React, { useState, useRef } from 'react';
+import './Slider.css';
+
 const ImageSlider = () => {
     const sliderRef = useRef(null);
+    const [modalVisible, setModalVisible] = useState(false);
+    const [selectedImage, setSelectedImage] = useState('');
 
     const scrollLeft = () => {
         if (sliderRef.current) {
@@ -15,22 +18,35 @@ const ImageSlider = () => {
         }
     };
 
+    const openModal = (imgSrc) => {
+        setSelectedImage(imgSrc);
+        setModalVisible(true);
+    };
+
+    const closeModal = () => {
+        setModalVisible(false);
+    };
+
     return (
         <div className="container">
             <img className="control prev" src="https://i.postimg.cc/W47WzSmc/img1.png" alt="Scroll Left" onClick={scrollLeft} />
             <div className="slider" ref={sliderRef}>
-                <img src="https://i.postimg.cc/3Nzww1RC/Rutas-X-101.png" alt="Pretroncal" />
-                <img src="https://i.postimg.cc/c4MHWRWY/Rutas-X-102.png" alt="Pretroncal" />
-                <img src="https://i.postimg.cc/htsfL93g/Rutas-X103-Act-Min.png" alt="Pretroncal" />
-                <img src="https://i.postimg.cc/zDhvtmPx/Rutas-X-104.png" alt="Pretroncal"/>
-                <img src="https://i.postimg.cc/prKdp8nw/Rutas-X-105.png" alt="Pretroncal" />
-                <img src="https://i.postimg.cc/7Zw5JB9m/Rutas-X-106.png" alt="Pretroncal" />
-                <img src="https://i.postimg.cc/5yzXLTDk/RT100E.png" alt="Troncal" />
-                <img src="https://i.postimg.cc/6642k4TL/RT101.png1&w=500" alt="Troncal" />
+                {['https://i.postimg.cc/3Nzww1RC/Rutas-X-101.png', 'https://i.postimg.cc/c4MHWRWY/Rutas-X-102.png', 'https://i.postimg.cc/htsfL93g/Rutas-X103-Act-Min.png', 'https://i.postimg.cc/zDhvtmPx/Rutas-X-104.png', 'https://i.postimg.cc/prKdp8nw/Rutas-X-105.png', 'https://i.postimg.cc/7Zw5JB9m/Rutas-X-106.png', 'https://i.postimg.cc/5yzXLTDk/RT100E.png', 'https://i.postimg.cc/6642k4TL/RT101.png'].map((src, index) => (
+                    <img key={index} src={src} alt="Rutas" onClick={() => openModal(src)} />
+                ))}
             </div>
             <img className="control next" src="https://i.postimg.cc/hjPCdxMz/img2.png" alt="Scroll Right" onClick={scrollRight} />
+            {modalVisible && (
+                <div className="modal">
+                    <span className="close" onClick={closeModal}>&times;</span>
+                    <img className="modal-content" src={selectedImage} alt="Expanded view" />
+                </div>
+            )}
         </div>
     );
 };
 
 export default ImageSlider;
+
+
+
